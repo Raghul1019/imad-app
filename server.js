c4b1bc217e9dd8aeaf33dbf0f193841c385ae59a
Raghul1019 <31257014+Raghul1019@users.noreply.github.com>
2017-08-31 22:18:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.get('/article-two', function (req, res) {
 app.get('/article-three', function (req, res) {
     res.send('idhu dhan da moonavadhu article uh');
 });
+app.get('/image', function(res, req) {
+    res.sendFile(path.join(_dirname,'ui', 'my.png' ));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

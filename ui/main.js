002aacb946db802e661846e68537674f51eb7275
Raghul1019 <31257014+Raghul1019@users.noreply.github.com>
2017-09-21 20:13:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,9 @@ var element =document.getElementById('raghul');
 var img = document.getElementById('f');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft=marginLeft +10;
+    marginLeft=marginLeft +1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
-  var interval = setInterval(moveRight,100);   
+  var interval = setInterval(moveRight,50);   
 };
